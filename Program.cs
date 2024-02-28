d5a098d0ae44fa47d18dd952696d80c05f9ae30a
GarimaDubey1999 <garimadubey@usf.edu>
2024-02-28 11:24:37 -0500

    Updated Code with comments

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
-﻿
-    public class Program
+﻿/*
+    Name:Garima Dubey
+    Deparment:Business Analytics & Information Systems
+*/
+public class Program
     {
         static void Main(string[] args)
         {
+            // Calling the Bubble Sort  Function
             int[] returnedArray = BubbleSort([5,72,3,14,7,1]);
 
             // Printing the returned array
@@ -13,13 +17,13 @@
         }
     
 
-        public static int[] BubbleSort(int[] array_to_be_sorted)
+         static int[] BubbleSort(int[] array_to_be_sorted)
         {
             for (int i = 0;i< array_to_be_sorted.Length; i++)
             {
-                for(int j = 0; j < array_to_be_sorted.Length - 1; j++)
+                for(int j = 0; j < array_to_be_sorted.Length - 1-i; j++)
                 {
-                    if (array_to_be_sorted[j] > array_to_be_sorted[j+1] && j != array_to_be_sorted.Length - 1)
+                    if (array_to_be_sorted[j] > array_to_be_sorted[j+1] )
                     {   
                         int temp = array_to_be_sorted[j];
                         array_to_be_sorted[j] = array_to_be_sorted[j + 1];
