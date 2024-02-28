7c8072cd25d1b6fc27834ae842362d677c2b52f3
GarimaDubey1999 <garimadubey@usf.edu>
2024-02-28 11:14:49 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] returnedArray = BubbleSort([5,72,3,14,7,1]);
+
+            // Printing the returned array
+            foreach (int num in returnedArray)
+            {
+                Console.WriteLine(num + " ");
+            }
+        }
+    
+
+        public static int[] BubbleSort(int[] array_to_be_sorted)
+        {
+            for (int i = 0;i< array_to_be_sorted.Length; i++)
+            {
+                for(int j = 0; j < array_to_be_sorted.Length - 1; j++)
+                {
+                    if (array_to_be_sorted[j] > array_to_be_sorted[j+1] && j != array_to_be_sorted.Length - 1)
+                    {   
+                        int temp = array_to_be_sorted[j];
+                        array_to_be_sorted[j] = array_to_be_sorted[j + 1];
+                        array_to_be_sorted[j + 1] = temp;
+                    }
+                }
+            }
+
+            return array_to_be_sorted;
+        }
+    }
+
